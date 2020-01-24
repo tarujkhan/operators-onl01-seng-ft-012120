50e1ad22fd9f247223d124a31a816cca45f78a39
taroj khan <tarujkhan@yahoo.com>
2020-01-24 00:36:50 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,13 +3,14 @@
 #end
 
 
-#def not_safe?(speed)
+def not_safe?(speed)
+speed < 40 || speed > 60 ? true:false
+end
 	
-#end
-	
-def unsafe?
+def unsafe?(speed)
   if speed < 40 || speed > 60
 true
   else 
   false
+end
 end
